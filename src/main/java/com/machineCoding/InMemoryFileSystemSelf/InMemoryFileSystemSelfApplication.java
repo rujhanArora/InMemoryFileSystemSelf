b7157419c753e954f3a71d43b2bfeb5ae3a81e44
rujhanArora <rujhanarora.96@gmail.com>
2021-09-11 15:37:13 +0530

    init with spring boot

---
+++ src/main/java/com/machineCoding/InMemoryFileSystemSelf/InMemoryFileSystemSelfApplication.java
@@ -0,0 +1,13 @@
+package com.machineCoding.InMemoryFileSystemSelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMemoryFileSystemSelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InMemoryFileSystemSelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/machineCoding/InMemoryFileSystemSelf/InMemoryFileSystemSelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machineCoding.InMemoryFileSystemSelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InMemoryFileSystemSelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
